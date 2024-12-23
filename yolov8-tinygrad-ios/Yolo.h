b99e9d10c6e4f085530c274e40006ebdcb780f57
roryclear <roryclear.rc@gmail.com>
2024-12-23 22:24:15 +0000

    new yolo file

---
+++ yolov8-tinygrad-ios/Yolo.h
@@ -0,0 +1,4 @@
+#import <Foundation/Foundation.h>
+
+CGFloat intersectionBetweenBox(NSArray *box1, NSArray *box2);
+NSMutableDictionary<NSString *, id> *extractValues(NSString *x);
---
+++ yolov8-tinygrad-ios/Yolo.m
@@ -0,0 +1,33 @@
+#import <Foundation/Foundation.h>
+#import "Yolo.h"
+
+CGFloat intersectionBetweenBox(NSArray *box1, NSArray *box2) {
+    CGFloat x1 = MAX([box1[0] floatValue], [box2[0] floatValue]);
+    CGFloat y1 = MAX([box1[1] floatValue], [box2[1] floatValue]);
+    CGFloat x2 = MIN([box1[2] floatValue], [box2[2] floatValue]);
+    CGFloat y2 = MIN([box1[3] floatValue], [box2[3] floatValue]);
+    return MAX(0, x2 - x1) * MAX(0, y2 - y1);
+}
+
+NSMutableDictionary<NSString *, id> *extractValues(NSString *x) {
+    NSMutableDictionary<NSString *, id> *values = [@{@"op": [x componentsSeparatedByString:@"("][0]} mutableCopy];
+    NSDictionary<NSString *, NSString *> *patterns = @{@"name": @"name='([^']+)'",@"datahash": @"datahash='([^']+)'",@"global_sizes": @"global_size=\\(([^)]+)\\)",
+        @"local_sizes": @"local_size=\\(([^)]+)\\)",@"bufs": @"bufs=\\(([^)]+)\\)",@"vals": @"vals=\\(([^)]+)\\)",
+        @"buffer_num": @"buffer_num=(\\d+)",@"size": @"size=(\\d+)"};
+    [patterns enumerateKeysAndObjectsUsingBlock:^(NSString *key, NSString *pattern, BOOL *stop) {
+        NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:pattern options:0 error:nil];
+        NSTextCheckingResult *match = [regex firstMatchInString:x options:0 range:NSMakeRange(0, x.length)];
+        if (match) {
+            NSString *contents = [x substringWithRange:[match rangeAtIndex:1]];
+            NSMutableArray<NSString *> *extracted_values = [NSMutableArray array];
+            for (NSString *value in [contents componentsSeparatedByString:@","]) {
+                NSString *trimmed_value = [value stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+                if (trimmed_value.length > 0) {
+                    [extracted_values addObject:trimmed_value];
+                }
+            }
+            values[key] = [extracted_values copy];
+        }
+    }];
+    return values;
+}
